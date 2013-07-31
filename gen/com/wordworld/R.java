cd7aeff1a5f6c3942f2d76eefb6ff89ae2edacf5
pppbk <saint_lucia@hanmail.net>
2013-07-31 21:08:08 +0900

    test

--- gen/com/wordworld/R.java
+++ gen/com/wordworld/R.java
@@ -22,15 +22,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int benk=0x7f080000;
+        public static final int bmp=0x7f080002;
+        public static final int bmw=0x7f080001;
+        public static final int bset=0x7f080003;
     }
     public static final class layout {
         public static final int index=0x7f030000;
     }
     public static final class menu {
         public static final int index=0x7f070000;
+        public static final int word_world=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
